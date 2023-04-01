2acf839951a8b0fca52c8fd669f78bee8d39497d
im-09mi <116474060+im-09mi@users.noreply.github.com>
2023-04-01 13:47:12 +0900

    20230401

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if(\App::environment(['production'])){
+            \URL::forseScheme('https');
+        }
      
     }
 }
